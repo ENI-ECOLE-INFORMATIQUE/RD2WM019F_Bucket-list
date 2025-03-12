6bc52c324ed767c2b5395732621d5d50017fa0a8
dsanchez <dsanchez@campus-eni.fr>
2025-03-12 10:16:52 +0100

    Module 04 TP1 ajout de About US au niveau due twig about_us.html.twig et ajout dans le MainController.php de la fonction about()

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,4 +13,10 @@ final class MainController extends AbstractController
     {
         return $this->render('main/index.html.twig', []);
     }
+
+    #[Route('/about-us', name: 'main_about_us', methods: ['GET'])]
+    public function about(): Response
+    {
+        return $this->render('main/about_us.html.twig', []);
+    }
 }
