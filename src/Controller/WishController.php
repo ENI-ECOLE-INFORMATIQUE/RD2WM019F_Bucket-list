92515042faae2bd9dedfd7c9db6486d3c80e8488
dsanchez <dsanchez@campus-eni.fr>
2025-03-13 12:15:29 +0100

    Module 05 TP1 Mise en place de la BDD. Creation de l'entite Wish Liste des wishes et détail d'un wish.

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,14 +11,22 @@ use Symfony\Component\Routing\Attribute\Route;
 class WishController extends AbstractController
 {
     #[Route('/wishes', name: 'wish_list', methods: ['GET'])]
-    public function list(): Response
+    public function list(WishRepository $wishrepository): Response
     {
-        return $this->render('wish/list.html.twig', []);
+        //Récupére les wish publiés, du plus récent au plus ancien
+        $wishes = $wishrepository->findBy(['isPublished' => true], ['dateCreated' => 'DESC']);
+        return $this->render('wish/list.html.twig', ["wishes"=>$wishes]);
     }
 
     #[Route('/wishes/{id}', name: 'wish_detail',requirements: ['id' => '\d+'], methods: ['GET'])]
-    public function detail(int $id): Response
+    public function detail(int $id, WishRepository $wishrepository): Response
+    // public function detail(Wish $wish, WishRepository $wishrepository): Response
     {
-        return $this->render('wish/detail.html.twig', []);
+        //Récupère ce wish en fonction de l'id présent dans l'url
+        $wish = $wishrepository->find($id);
+        if(!$wish){
+            throw $this->createNotFoundException('This wish do not exists! Sorry!');
+        }
+        return $this->render('wish/detail.html.twig', ["wish"=>$wish]);
     }
 }
