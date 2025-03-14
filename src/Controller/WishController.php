8f5e225aa34e4728a7595b2915e1ca8b6fd5ffa2
dsanchez <dsanchez@campus-eni.fr>
2025-03-14 17:04:09 +0100

    Module 06 TP1 Mise en place des formulaire pour la création modification et suppression d'un wish. Encore un bug sur le delete

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Wish;
+use App\Form\WishType;
 use App\Repository\WishRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -28,5 +32,71 @@ class WishController extends AbstractController
             throw $this->createNotFoundException('This wish do not exists! Sorry!');
         }
         return $this->render('wish/detail.html.twig', ["wish"=>$wish]);
+    }
+    #[Route('/wishes/create', name: 'wish_create', methods: ['GET','POST'])]
+    public function create(Request $request, EntityManagerInterface  $em): Response{
+        //Création de l'entité vide
+        $wish = new Wish();
+        //Création du formulaire et association de l'entité vide.
+        $wishForm = $this->createForm(WishType::class, $wish);
+        //Récupère les données du formulaire et on les injecte dans notre $wish.
+        $wishForm->handleRequest($request);
+        //On vérifie si le formulaire a été soumis et que les données soumises sont valides.
+        if($wishForm->isSubmitted() && $wishForm->isValid()){
+            //Hydrater les propriétés absentes du formulaire
+            $wish->setIsPublished(true);
+            //Sauvegarde dans la Bdd
+            $em->persist($wish);
+            $em->flush();
+            //Affiche un message à l'utilisateur sur la prochaine page.
+            $this->addFlash('success', 'Your wish has been created!');
+            //Redirige vers la page de detail du wish
+            return $this->redirectToRoute('wish_detail', ['id'=>$wish->getId()]);
+        }
+        //Affiche le formulaire
+        return $this->render('wish/create.html.twig', ["wishForm"=>$wishForm]);
+    }
+
+
+    #[Route('/wishes/{id}/update', name: 'wish_update',requirements: ['id' => '\d+'], methods: ['GET','POST'])]
+    public function update(int $id,WishRepository  $wishrepository,Request $request, EntityManagerInterface  $em): Response{
+        //Récupération de l'entité wish  en fonction de son id.
+        $wish = $wishrepository->find($id);
+        //s'il n'existe pas dans la bdd, on lance une erreur 404
+        if(!$wish){
+            throw $this->createNotFoundException('This wish do not exists! Sorry!');
+        }
+        //Création du formulaire et association de l'entité .
+        $wishForm = $this->createForm(WishType::class, $wish);
+        //Récupère les données du formulaire et on les injecte dans notre $wish.
+        $wishForm->handleRequest($request);
+        //On vérifie si le formulaire a été soumis et que les données soumises sont valides.
+        if($wishForm->isSubmitted() && $wishForm->isValid()){
+            //Hydrater les propriétés absentes du formulaire
+            $wish->setDateUpdated(new \DateTimeImmutable());
+            //Sauvegarde dans la Bdd
+            $em->flush();
+            //Affiche un message à l'utilisateur sur la prochaine page.
+            $this->addFlash('success', 'Your wish has been updated!');
+            //Redirige vers la page de detail du wish
+            return $this->redirectToRoute('wish_detail', ['id'=>$wish->getId()]);
+        }
+        //Affiche le formulaire
+        return $this->render('wish/create.html.twig', ["wishForm"=>$wishForm]);
+    }
+    #[Route('/wishes/{id}/delete', name: 'wish_delete',requirements: ['id' => '\d+'], methods: ['GET'])]
+     public function delete(int $id,WishRepository  $wishrepository, Request $request, EntityManagerInterface  $em): Response{
+        $wish = $wishrepository->find($id);
+        //s'il n'existe pas dans la bdd, on lance une erreur 404
+        if(!$wish){
+            throw $this->createNotFoundException('This wish do not exists! Sorry!');
+        }
+        if($this->isCsrfTokenValid('delete'.$wish->getId(), $request->request->get('_token'))){
+            $em->remove($wish,true);
+             $this->addFlash('success', 'Your wish has been deleted!');
+        }else{
+            $this->addFlash('danger', 'Your wish cannot be deleted!');
+        }
+        return $this->redirectToRoute('wish_list');
     }
 }
