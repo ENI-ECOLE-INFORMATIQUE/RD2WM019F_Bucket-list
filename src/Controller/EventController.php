42a46da50f9f76da8653760f6047dab76325071b
stropee <stropee@campus-eni.fr>
2025-03-21 12:33:51 +0100

    TP 10.1

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Form\EventSearchType;
+use App\Form\Models\EventSearch;
 use App\Models\Event;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
@@ -13,17 +16,35 @@ final class EventController extends AbstractController
 {
     #[Route('/events', name: 'event_list')]
     public function list(
-        DenormalizerInterface $denormalizer
+        DenormalizerInterface $denormalizer,
+        Request               $request
     ): Response
     {
-        $events = file_get_contents("https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/evenements-publics-openagenda/records?limit=20");
+        $url = "https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/evenements-publics-openagenda/records?limit=20";
+
+        $eventSearch = new EventSearch();
+        $eventSearchForm = $this->createForm(EventSearchType::class, $eventSearch);
+
+        $eventSearchForm->handleRequest($request);
+
+        if ($eventSearchForm->isSubmitted()) {
+            if ($eventSearch->getCityName()) {
+                $url .= "&refine.location_city=" . $eventSearch->getCityName();
+            }
+            if ($eventSearch->getStartDate()) {
+                $url .= "&refine.firstdate_begin=" . $eventSearch->getStartDate()->format("Y-m-d");
+            }
+        }
+
+        $events = file_get_contents($url);
         $events = json_decode($events, true);
         $events = $denormalizer->denormalize($events['results'], Event::class . '[]');
 
         dump($events);
 
         return $this->render('event/list.html.twig', [
-            'events' => $events
+            'events' => $events,
+            'eventSearchForm' => $eventSearchForm
         ]);
     }
 }
---
+++ src/Form/Models/EventSearch.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form\Models;
+
+class EventSearch
+{
+
+    private ?\DateTimeInterface $startDate;
+    private ?string $cityName;
+
+    public function getStartDate(): ?\DateTimeInterface
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(?\DateTimeInterface $startDate): void
+    {
+        $this->startDate = $startDate;
+    }
+
+    public function getCityName(): ?string
+    {
+        return $this->cityName;
+    }
+
+    public function setCityName(?string $cityName): void
+    {
+        $this->cityName = $cityName;
+    }
+
+
+
+
+
+}
